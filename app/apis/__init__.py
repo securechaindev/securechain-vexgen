848bcbd16d5afef11aa00f489f3e078a31d7a758
GermanMT <germanoctako@gmail.com>
2024-08-01 17:56:36 +0200

    feat: Add frontend to docker compose

--- app/apis/__init__.py
+++
@@ -1,7 +0,0 @@
-from .github_service import get_last_commit_date_github
-from .manager_service import get_all_versions
-
-__all__ = [
-    "get_all_versions",
-    "get_last_commit_date_github"
-]
--- app/utils/get_first_pos.py
+++
@@ -1,7 +0,0 @@
-async def get_first_position(data: str, operators: list[str]) -> int:
-    if not any(operator in data for operator in operators):
-        return len(data)
-    for index, char in enumerate(data):
-        if char in operators:
-            return index
-    return 0

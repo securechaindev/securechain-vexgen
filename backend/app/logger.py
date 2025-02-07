754ddf961f625b93f78bf005a4aebdac9ba73fc3
GermanMT <germanoctako@gmail.com>
2025-02-07 21:00:42 +0100

    chore: Change logger to rotating file handler

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -1,13 +1,16 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 uvicorn_access = logging.getLogger("uvicorn.access")
 uvicorn_access.disabled = True
 
 logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.getLevelName(logging.DEBUG))
+logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-error_file_handler = logging.FileHandler("errors.log")
-error_file_handler.setLevel(logging.DEBUG)
-error_file_handler.setFormatter(formatter)
-logger.addHandler(error_file_handler)
+
+rotating_handler = RotatingFileHandler("errors.log", maxBytes=5 * 1024 * 1024, backupCount=3)
+rotating_handler.setLevel(logging.DEBUG)
+rotating_handler.setFormatter(formatter)
+
+logger.addHandler(rotating_handler)

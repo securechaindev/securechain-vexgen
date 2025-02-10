9bbaeac04751c3c67aabc20d6dae07fd2edd8b09
GermanMT <germanoctako@gmail.com>
2025-02-10 18:14:46 +0100

    feat: Update vuln and graph seeds

--- seeds/graphdb_seeder.sh
+++ seeds/graphdb_seeder.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-export $(grep -v '^#' .env | xargs)
+DB_PATH="/var/lib/neo4j/data/databases/neo4j"
 
-docker exec --interactive --tty pypi_neo4j gzip -d /backups/pypi_neo4j.cypher.gz  > /dev/null 2>&1
-docker exec --interactive --tty pypi_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_PYPI -a bolt://localhost:7687 -f /backups/pypi_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty pypi_neo4j rm /backups/pypi_neo4j.cypher
-echo "PyPI graph database was restored"
-docker exec --interactive --tty npm_neo4j gzip -d /backups/npm_neo4j.cypher.gz  > /dev/null 2>&1
-docker exec --interactive --tty npm_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_NPM -a bolt://localhost:7686 -f /backups/npm_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty npm_neo4j rm /backups/npm_neo4j.cypher
-echo "NPM graph database was restored"
-docker exec --interactive --tty maven_neo4j gzip -d /backups/maven_neo4j.cypher.gz  > /dev/null 2>&1
-docker exec --interactive --tty maven_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_MAVEN -a bolt://localhost:7685 -f /backups/maven_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty maven_neo4j rm /backups/maven_neo4j.cypher
-echo "Maven graph database was restored"
-docker exec --interactive --tty cargo_neo4j gzip -d /backups/cargo_neo4j.cypher.gz  > /dev/null 2>&1
-docker exec --interactive --tty cargo_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_CARGO -a bolt://localhost:7684 -f /backups/cargo_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty cargo_neo4j rm /backups/cargo_neo4j.cypher
-echo "Cargo graph database was restored"
-docker exec --interactive --tty nuget_neo4j gzip -d /backups/nuget_neo4j.cypher.gz  > /dev/null 2>&1
-docker exec --interactive --tty nuget_neo4j cypher-shell -u $GRAPH_DB_USER -p $GRAPH_DB_PASSWORD_NUGET -a bolt://localhost:7683 -f /backups/nuget_neo4j.cypher > /dev/null 2>&1
-docker exec --interactive --tty nuget_neo4j rm /backups/nuget_neo4j.cypher
-echo "NuGet graph database was restored"
+if [ -d "$DB_PATH" ] && [ "$(ls -A "$DB_PATH")" ]; then
+    echo "The database is not empty, no data will be imported"
+else
+    neo4j-admin database load neo4j --from-path=/backups
+fi
+
+neo4j console
--- seeds/vulndb_seeder.sh
+++ seeds/vulndb_seeder.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-IS_EMPTY=$(mongosh --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb nvd --eval "db.getCollection('cves').countDocuments({}) === 0")
+HAVE_DATA=$(mongosh --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb nvd --eval "db.getCollection('cves').countDocuments({}) != 0")
 
-if [ $IS_EMPTY == true ]; then
-  mongorestore --nsInclude=nvd.cves --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
-  mongorestore --nsInclude=nvd.cpe_matchs --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
-  mongorestore --nsInclude=nvd.cpe_products --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
-  mongorestore --nsInclude=nvd.cpes --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
-  mongorestore --nsInclude=nvd.cwes --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
-  mongorestore --nsInclude=vulners_db.exploits --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
-  mongorestore --nsInclude=depex.env_variables --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip ./seeds/vuln/
+if [ $HAVE_DATA == true ]; then
+    echo "The database is not empty, no data will be imported"
 else
-  echo "The database is not empty, no data will be imported"
+    mongorestore --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip --nsInclude=nvd.* --dir=./seeds/vuln/
+    mongorestore --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip --nsInclude=vulners_db.* --dir=./seeds/vuln/
+    mongorestore --username $VULN_DB_USER --password $VULN_DB_PASSWORD --authenticationDatabase admin --host mongodb --gzip --nsInclude=depex.* --dir=./seeds/vuln/
 fi

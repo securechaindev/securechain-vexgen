c904c3851072be5df7bd2ad5b8dcb285c9d0d18a
GermanMT <germanoctako@gmail.com>
2025-02-10 18:16:03 +0100

    feat: Improve maven service using json sonatype api

--- backend/app/apis/managers/maven_service.py
+++ backend/app/apis/managers/maven_service.py
@@ -2,30 +2,26 @@ from asyncio import TimeoutError, sleep
 from typing import Any
 
 from aiohttp import ClientConnectorError, ClientSession
-from xmltodict import parse
 
 
 async def get_maven_versions(
-    package_artifact_id: str, package_group_id: str
+    group_id: str, artifact_id: str
 ) -> list[dict[str, Any]]:
     versions: list[dict[str, Any]] = []
-    api_url = f"https://repo1.maven.org/maven2/{package_group_id.replace(".", "/")}/{package_artifact_id}/maven-metadata.xml"
-    async with ClientSession() as session:
-        while True:
-            try:
-                async with session.get(api_url) as response:
-                    xml_string = await response.text()
-                    break
-            except (ClientConnectorError, TimeoutError):
-                await sleep(5)
-    try:
-        pom_dict = parse(xml_string)
-    except Exception as _:
-        return versions
-    if isinstance(pom_dict["metadata"]["versioning"]["versions"]["version"], list):
-        raw_versions = pom_dict["metadata"]["versioning"]["versions"]["version"]
-    else:
-        raw_versions = [pom_dict["metadata"]["versioning"]["versions"]["version"]]
-    for count, version in enumerate(raw_versions):
-        versions.append({"name": version, "count": count})
+    start = 0
+    docs = True
+    while docs:
+        api_url = f"https://search.maven.org/solrsearch/select?q=g:{group_id}+AND+a:{artifact_id}&core=gav&rows=200&start={start}"
+        async with ClientSession() as session:
+            while True:
+                try:
+                    async with session.get(api_url) as response:
+                        response = await response.json()
+                        break
+                except (ClientConnectorError, TimeoutError):
+                    await sleep(5)
+        start += 200
+        docs = response.get("response").get("docs", [])
+        for count, version in enumerate(response.get("response").get("docs", [])):
+            versions.append({"name": version.get("v"), "count": count})
     return versions

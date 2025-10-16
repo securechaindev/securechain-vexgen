95cbbfa895b199c5ec908c9b2f2f8f65344ada33
GermanMT <germanoctako@gmail.com>
2025-10-16 12:48:30 +0200

    feat: improve logger class

--- app/logger.py
+++ app/logger.py
@@ -1,16 +1,51 @@
-import logging
+from functools import lru_cache
+from logging import INFO, Formatter, getLogger
 from logging.handlers import RotatingFileHandler
+from pathlib import Path
 
-uvicorn_access = logging.getLogger("uvicorn.access")
-uvicorn_access.disabled = True
 
-logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.DEBUG)
+class LoggerManager:
+    def __init__(
+        self,
+        log_file: str = "errors.log",
+        max_bytes: int = 5 * 1024 * 1024,
+        backup_count: int = 5,
+    ):
+        self.logger = getLogger("securechain")
+        self.logger.setLevel(INFO)
+        self.logger.propagate = False
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        log_path = Path(log_file)
+        log_path.parent.mkdir(parents=True, exist_ok=True)
 
-rotating_handler = RotatingFileHandler("errors.log", maxBytes=5 * 1024 * 1024, backupCount=3)
-rotating_handler.setLevel(logging.DEBUG)
-rotating_handler.setFormatter(formatter)
+        file_handler = RotatingFileHandler(
+            log_file, maxBytes=max_bytes, backupCount=backup_count, encoding="utf-8"
+        )
+        formatter = Formatter(
+            "%(asctime)s - %(levelname)s - %(name)s - %(filename)s:%(lineno)d - %(message)s"
+        )
+        file_handler.setFormatter(formatter)
 
-logger.addHandler(rotating_handler)
+        if not self.logger.handlers:
+            self.logger.addHandler(file_handler)
+
+    def info(self, msg: str, *args, **kwargs):
+        self.logger.info(msg, *args, **kwargs)
+
+    def warning(self, msg: str, *args, **kwargs):
+        self.logger.warning(msg, *args, **kwargs)
+
+    def error(self, msg: str, *args, **kwargs):
+        self.logger.error(msg, *args, **kwargs)
+
+    def exception(self, msg: str, *args, **kwargs):
+        self.logger.exception(msg, *args, **kwargs)
+
+    def debug(self, msg: str, *args, **kwargs):
+        self.logger.debug(msg, *args, **kwargs)
+
+@lru_cache
+def get_logger() -> LoggerManager:
+    return LoggerManager("logs/errors.log")
+
+logger: LoggerManager = get_logger()

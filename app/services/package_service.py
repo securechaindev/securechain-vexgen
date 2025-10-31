a3652a3469677d5322709e2f87130f3c0772bf13
GermanMT <germanoctako@gmail.com>
2025-10-31 18:27:28 +0100

    feat: improve service responses

--- app/services/package_service.py
+++ app/services/package_service.py
@@ -8,7 +8,8 @@ class PackageService:
     async def read_package_by_name(self, node_type: str, name: str) -> tuple[str, list[str]]:
         query = f"""
         MATCH(p:{node_type}{{name:$name}})
-        RETURN p.name, coalesce(p.import_names, []) + [p.name] + [coalesce(p.group_id, '')]
+        RETURN p.name AS package_name,
+               coalesce(p.import_names, []) + [p.name] + [coalesce(p.group_id, '')] AS import_names
         """
         async with self.driver.session() as session:
             result = await session.run(
@@ -16,4 +17,4 @@ class PackageService:
                 name=name
             )
             record = await result.single()
-            return (record[0], record[1]) if record else (None, None)
+            return (record.get("package_name"), record.get("import_names")) if record else (None, None)
--- app/services/version_service.py
+++ app/services/version_service.py
@@ -11,21 +11,21 @@ class VersionService:
         query = f"""
         MATCH (p:{node_type}{{name:$name}})
         MATCH (p)-[r:HAVE]->(v: Version) WHERE v.name = $version
-        RETURN v.vulnerabilities
+        RETURN v.vulnerabilities AS vulnerability_ids
         """
         async with self.driver.session() as session:
             result = await session.run(query, name=name, version=version)
             record = await result.single()
-        return record[0] if record else []
+        return record.get("vulnerability_ids") if record else []
 
 
     async def read_versions_names_by_package(self, node_type: str, name: str) -> list[str]:
         query = f"""
         MATCH (p:{node_type}{{name:$name}})
         MATCH (p)-[r:HAVE]->(v: Version)
-        RETURN collect(v.name)
+        RETURN collect(v.name) AS versions
         """
         async with self.driver.session() as session:
             result = await session.run(query, name=name)
             record = await result.single()
-        return record[0] if record else None
+        return record.get("versions") if record else None

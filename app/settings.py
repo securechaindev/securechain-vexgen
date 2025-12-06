59bf67be7ab6af8f6e7d0409e1c3b4687eef40bc
GermanMT <germanoctako@gmail.com>
2025-12-06 23:34:42 +0100

    fix: add extra ignore parameter in model config to pass away from unrequired config vars

--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
     # Database connections (required)
     GRAPH_DB_URI: str = Field(..., alias="GRAPH_DB_URI")

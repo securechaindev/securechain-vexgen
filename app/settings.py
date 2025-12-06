7a31a5acccc04efefb6b45c31b2a1d40d69eb01a
GermanMT <germanoctako@gmail.com>
2025-12-06 23:52:44 +0100

    chore: add type ignore to get settings function

--- app/settings.py
+++ app/settings.py
@@ -73,7 +73,7 @@ class Settings(BaseSettings):
 
 @lru_cache
 def get_settings() -> Settings:
-    return Settings()
+    return Settings() # type: ignore[call-arg]
 
 
 settings: Settings = get_settings()
